8ca1ecee7e76ee781e34f3233127dc5f11181f35
bdffgee <bdffgee@gmail.com>
2021-08-01 20:15:40 +0800

    hotFix test1~

--- eureka_order/src/main/java/all/eureka_order/controller/OrderController.java
+++ eureka_order/src/main/java/all/eureka_order/controller/OrderController.java
@@ -47,6 +47,7 @@ public class OrderController {
     })*/
     public String feignQuery(@PathVariable Integer uid){
         String str = iUserClient.testQuery(uid);
+        System.out.println("hotFix test~");
         return "Version: 1 OpenFeign订单里调用的:"+str;
     }
 
